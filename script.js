4f5fb556e0b4fe25199fd73008e5a8c285dcc36d
Mener7584 <138903594+Mener7584@users.noreply.github.com>
2024-06-29 21:18:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,12 @@
     document.body.classList.toggle("lock");
   });
 })();
+
+const header = document.querySelector(".header");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
